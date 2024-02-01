9761df760d64d17fc7d8f91d2ac2e6d8273447b6
tucker <tucker961220@gmail.com>
2024-02-01 12:52:55 +0800

    api

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from function import *
 import os
 
-#---遠端爬取最新pdf---
+# 遠端爬取最新pdf
 latestData = getCompleteData(0)
 print(latestData)
 
+# 確保目錄存在
 current_file_path = os.path.abspath(__file__)
 current_directory = os.path.dirname(current_file_path)
 target_folder_path = os.path.join(current_directory, "API", "meals")
+os.makedirs(target_folder_path, exist_ok=True)
+
+# 使用絕對路徑
 file_path = os.path.join(target_folder_path, "latest.json")
 with open(file_path, 'w') as file:
     file.write(latestData)
+
+
 
 
