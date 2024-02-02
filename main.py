34f0399e3096ddc243c86035995b39821ff390ef
tucker <tucker961220@gmail.com>
2024-02-02 20:59:42 +0800

    新增post到後端

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from function import *
 import os
+import requests
 
 # 遠端爬取最新pdf
 latestData = getCompleteData(0)
@@ -11,11 +12,16 @@ current_directory = os.path.dirname(current_file_path)
 target_folder_path = os.path.join(current_directory, "API", "meals")
 os.makedirs(target_folder_path, exist_ok=True)
 
-# 使用絕對路徑
+# 寫入新資料
 file_path = os.path.join(target_folder_path, "latest.json")
 with open(file_path, 'w') as file:
     file.write(latestData)
 
-
-
-
+# 發送資料給後端
+url = ""    # 到時候設置github token
+token = ""  # 到時候設置github token
+postData = {
+    "token": "",
+    "mealsData": latestData
+}
+# requests.post(url, postData)
