fd66c6cb7e10ce1e564ab211ca1753806b92d89b
tucker <tucker961220@gmail.com>
2024-02-15 09:43:30 +0800

    .

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ url = os.environ["meals_request_url"]    # 到時候設置github token
 token = os.environ["meals_request_token"]
 print(url, token)
 headers = {
-    "Authorization" : "Bearer " + token
+    "Authorization" : "Bearer " + token,
+    "Content-Type": "application/json"
 }
-req = requests.put(url, latestData, headers=headers)
+req = requests.put(url, data=latestData, headers=headers)
 print(req.status_code)
